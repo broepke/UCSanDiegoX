946d72fd182e276632d838c64a11f725598326e9
Brian Roepke <broepke@gmail.com>
2019-02-11 09:02:44 -0800

    Update counting.py

--- Prob-and-Stats/Topic3-Counting/counting.py
+++ Prob-and-Stats/Topic3-Counting/counting.py
@@ -1,11 +1,9 @@
 target = 2016
 counter = 0
 
-for i in range(target):
-    try:
-        if target % i == 0:
-            counter += 1
-    except:
-        counter =+ 1
+for i in range(1,target+1):
+    if target % i == 0:
+        counter += 1
+        print(i)
 
-print(counter)
+print('Total numbers divible by ' + str(target) + ' = ' + str(counter))
