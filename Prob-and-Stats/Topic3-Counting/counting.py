584a32b9d20febae3dfdbe6a7e2b5462672e9c02
Brian Roepke <broepke@gmail.com>
2019-02-11 12:54:01 -0800

    Update counting.py

--- Prob-and-Stats/Topic3-Counting/counting.py
+++ Prob-and-Stats/Topic3-Counting/counting.py
@@ -1,9 +1,26 @@
-target = 2016
-counter = 0
+# target = 2016
+# counter = 0
+#
+# for i in range(1,target+1):
+#     if target % i == 0:
+#         counter += 1
+#         print(i)
+#
+# print('Total numbers divible by ' + str(target) + ' = ' + str(counter))
 
-for i in range(1,target+1):
-    if target % i == 0:
-        counter += 1
-        print(i)
 
-print('Total numbers divible by ' + str(target) + ' = ' + str(counter))
+# Of 100 foreign journalists who can speak Chinese, English or French at a press conference:
+#
+# 60 speak Chinese.
+#
+# 65 speak English.
+#
+# 60 speak French.
+#
+# 35 speak both Chinese and English.
+#
+# 25 speak both Chinese and French.
+#
+# 35 speak both English and French.
+
+
