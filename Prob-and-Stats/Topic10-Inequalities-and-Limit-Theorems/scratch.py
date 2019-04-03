325193a0a395867e9134dd26812fd0560ee67b72
Brian Roepke <broepke@gmail.com>
2019-04-03 06:55:42 -0700

    Playing with Atom

--- Prob-and-Stats/Topic10-Inequalities-and-Limit-Theorems/scratch.py
+++ Prob-and-Stats/Topic10-Inequalities-and-Limit-Theorems/scratch.py
@@ -1,5 +1,6 @@
 import math
 import itertools
+import collections
 
 a = [1,2]
 b = [3,4]
@@ -10,3 +11,8 @@ print(product)
 print(math.e)
 
 print(math.sqrt(144))
+
+set_1 = {'A', 'B','C','D'}
+set_2 = {'C','D','E','F'}
+
+print(set_1.symmetric_difference(set_2))
