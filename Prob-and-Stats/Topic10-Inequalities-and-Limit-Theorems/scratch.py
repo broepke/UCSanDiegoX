326e89164dab8cea4c127ef5748125ef90da9023
Brian Roepke <broepke@gmail.com>
2019-04-04 20:41:48 -0700

    Playing

--- Prob-and-Stats/Topic10-Inequalities-and-Limit-Theorems/scratch.py
+++ Prob-and-Stats/Topic10-Inequalities-and-Limit-Theorems/scratch.py
@@ -1,6 +1,8 @@
 import math
 import itertools
 import collections
+import numpy as np
+import pandas as import pd
 
 a = [1,2]
 b = [3,4]
@@ -15,4 +17,7 @@ print(math.sqrt(144))
 set_1 = {'A', 'B','C','D'}
 set_2 = {'C','D','E','F'}
 
+print(set_1.union(set_2))
+print(set_1.intersection(set_2))
 print(set_1.symmetric_difference(set_2))
+print(len(set_1.union(set_2)))
