ec6d90bc4a407875b7b9510b965a1b497ea6871a
Brian Roepke <broepke@gmail.com>
2019-04-07 07:25:41 -0700

    HW 11 Complete

--- Prob-and-Stats/_Calculators/unbiased_estimators.py
+++ Prob-and-Stats/_Calculators/unbiased_estimators.py
@@ -9,16 +9,18 @@ n = len(list) # total number of samples
 mu = np.mean(list) # calculate the mean
 sigma = np.std(list)
 var = np.var(list)
+sem = sp.sem(list)
 
 print("mean = ", mu)
 print("std. dev. = ", sigma)
 print("raw variance ('S^2') = ", var)
+print('standard error of the mean = ', sem)
 
 # Calculate the "Unbiased Sample Variance"
 for i in list:
     total += (i-mu)**2
 
-print("Unbiased Sample Variace = ",total/(n-1))
+print("unbiased sample variace = ",total/(n-1))
 
 # Calculate it another way
 #print(var * (n/(n-1)))
