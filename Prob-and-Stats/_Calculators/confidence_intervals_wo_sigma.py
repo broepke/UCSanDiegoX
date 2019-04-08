Fixes for Variance using ddof=1 for Unbiased

Prob-and-Stats/_Calculators/confidence_intervals_wo_sigma.py
@@ -13,21 +13,16 @@ p = 0.90
 total = 0
 n = len(list)
 mu = np.mean(list)
-var = np.var(list)
+var = np.var(list, ddof=1)  # add ddof=1 for unbiased (Bessle Corrected)
 bounds = t.interval(p, len(list)-1, loc=np.mean(list), scale=sem(list))
 critical_t = t.ppf(((1+p)/2),n-1)
 
-# Calculate the "Unbiased Sample Variance" - BESSEL CORRECTED
-for i in list:
-    total += (i-mu)**2
-usv = total/(n-1)
-
-sigma_est = sqrt(usv)
+sigma_est = sqrt(var)
 std_error = critical_t * sigma_est / sqrt(n)
 
 print('Mean =', mu)
 print('Critical T =', critical_t)
-print('Unbiased Sample Variace (Bessel Corrected) = ', usv)
+print('Unbiased Sample Variace (Bessel Corrected) = ', var)
 print('Standard Deviation (Estimation) =', sigma_est)
 print('Standard Error =', std_error)
 print('Lower Bounds =', bounds[0])

Prob-and-Stats/_Calculators/unbiased_estimators.py
@@ -9,22 +9,12 @@ n = len(list) # total number of samples
 mu = np.mean(list) # calculate the mean
 sigma = np.std(list)
 var = np.var(list)
+var_u = np.var(list, ddof=1) #add ddof=1 for unbiased (Bessle Corrected)
 sem = sp.sem(list)
 
 print("mean = ", mu)
 print("std. dev. = ", sigma)
 print("raw variance ('S^2') = ", var)
+print("unbiased sample variace = ", var_u)
 print('standard error of the mean = ', sem)
 print("standard error = ", sigma/sqrt(n))
-
-# Calculate the "Unbiased Sample Variance"
-for i in list:
-    total += (i-mu)**2
-
-print("unbiased sample variace = ",total/(n-1))
-
-# Calculate it another way
-#print(var * (n/(n-1)))
-
-# Raw variance is also Std.Dev Squared
-# print(sigma**2)