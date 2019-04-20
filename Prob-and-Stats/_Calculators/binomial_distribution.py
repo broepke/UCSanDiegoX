a7cba428eb0a7917db90a1877f434197c1ea010a
Brian Roepke <broepke@gmail.com>
2019-04-19 19:48:18 -0700

    Updated Binom Dist to include PMF, CDF, PPF

--- Prob-and-Stats/_Calculators/binomial_distribution.py
+++ Prob-and-Stats/_Calculators/binomial_distribution.py
@@ -3,17 +3,25 @@ import numpy as np
 
 
 # Binomal Distribution
-n = 10
-p = 0.7
+n = 8
+k = 4
+p = 0.5
+q = 1 - p
 
 expect = binom.expect(args=(n, p))
-mean = binom.mean(n,p)
+mean = binom.mean(n, p)
 var = binom.var(n, p)
 sigma = binom.std(n, p)
-mode = np.floor((n+1)*p)
+mode = np.floor((n + 1) * p)
+pmf = binom.pmf(k, n, p)
+cdf = binom.cdf(k, n, p)
+ppf = binom.ppf(q, n, p)
 
-print('expected value = ',expect)
+print('expected value = ', expect)
 print('mean = ', mean)
 print('variance = ', var)
-print('std. dev. = ',sigma)
+print('std. dev. = ', sigma)
 print('mode = ', mode)
+print('pmf = ', pmf)
+print('cdf = ', cdf)
+print('ppf = ', ppf)
