b5e5c126da345aac97b6d7d12d7d79e02c2891db
Brian Roepke <broepke@gmail.com>
2019-05-04 18:39:06 -0700

    corrected the L1/2/inf distance calc

--- Machine-Learning/Topic1-Prediction-Problems/L0_L1_L2_Linf_Calcs.py
+++ Machine-Learning/Topic1-Prediction-Problems/L0_L1_L2_Linf_Calcs.py
@@ -1,21 +1,20 @@
 import numpy as np
 
-x = np.array([1, 2, -1, 3])
+# x = np.array([1, 2, -1, 3])
 
-# for a 2D array use this - you can use a 1D or 2D with this
-x = np.array([[1, 2], [-1, 3]])
-
-print(x)
+# Calculate the varius distances between two arrays (euclidian = L2)
+x = np.array([63.03, 22.55, 39.61, 40.48, 98.67, -0.25])
+y = np.array([43.92, 14.18, 37.83, 29.74, 134.46, 6.45])
 
 if x.ndim < 2:
     print("L0 length of the vector:")
-    print(np.linalg.norm(x, ord=0))
+    print(np.linalg.norm(x-y, ord=0),'\n')
 
 print("L1 length of the vector:")
-print(np.linalg.norm(x, ord=1))
+print(np.linalg.norm(x-y, ord=1),'\n')
 
 print("L2 length of the vector:")
-print(np.linalg.norm(x, ord=2))
+print(np.linalg.norm(x-y, ord=2),'\n')
 
 print("Infinity (max) length of the vector:")
-print(np.linalg.norm(x, np.inf))
+print(np.linalg.norm(x-y, np.inf))
