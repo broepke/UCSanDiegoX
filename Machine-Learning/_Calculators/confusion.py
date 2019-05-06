c484aa857cf84fd95aa509b21b7cc8535324cbe9
Brian Roepke <broepke@gmail.com>
2019-05-06 07:46:39 -0700

    Confusion Matrix Methods

--- Machine-Learning/_Calculators/confusion.py
+++ Machine-Learning/_Calculators/confusion.py
@@ -1,4 +1,7 @@
 import numpy as np
+from sklearn.metrics import confusion_matrix
+import pandas as pd
+import matplotlib.pyplot as plt
 
 x = [0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
      2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 0, 2, 0, 0, 0, 0, 0, 1, 0]
@@ -41,11 +44,41 @@ for i in range(len(testy)):
 
 confusion = np.array([row_no, row_dh, row_sl])
 
-# print(confusion)
+print('\n', 'iterating lists method')
+print(confusion)
 
 # here is how you can accomplish the same thing as above with np.add.at
 c = np.zeros((3, 3), dtype=int)
-
 np.add.at(c, (x, y), 1)
 
+print('\n', 'using the np.add.at method')
 print(c)
+
+
+d = confusion_matrix(x, y)
+
+print('\n', 'using the confusion matrix from scikit-learn')
+print(d)
+
+
+y_actu = pd.Series(x, name='Actual')
+y_pred = pd.Series(y, name='Predicted')
+df_confusion = pd.crosstab(y_actu, y_pred)
+
+print('\n', 'using pandas crostab')
+print(df_confusion)
+
+
+def plot_confusion_matrix(df_confusion, title='Confusion matrix', cmap=plt.cm.gray_r):
+    plt.matshow(df_confusion, cmap=cmap) # imshow
+    #plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(df_confusion.columns))
+    plt.xticks(tick_marks, df_confusion.columns, rotation=45)
+    plt.yticks(tick_marks, df_confusion.index)
+    #plt.tight_layout()
+    plt.ylabel(df_confusion.index.name)
+    plt.xlabel(df_confusion.columns.name)
+
+plot_confusion_matrix(df_confusion)
+plt.show()
