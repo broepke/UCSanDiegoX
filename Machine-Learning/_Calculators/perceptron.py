5cb5115cb33f656485f6e8b36865b6b9b4a6c9dc
Brian Roepke <broepke@gmail.com>
2019-06-23 17:23:46 -0700

    Topic 1 week 6

---
+++ Machine-Learning/_Calculators/perceptron.py
@@ -0,0 +1,9 @@
+from sklearn.datasets import load_digits
+from sklearn.linear_model import Perceptron
+import numpy as np
+
+X, Y = load_digits(return_X_y=True)
+clf = Perceptron(tol=1e-3, random_state=0)
+clf.fit(X, Y)
+
+print(clf.score(X, Y))
--- Machine-Learning/_Calculators/w_dot_x_plus_b.py
+++ Machine-Learning/_Calculators/w_dot_x_plus_b.py
@@ -1,19 +1,27 @@
 import numpy as np
 
-'''
-A linear classifier on  ℝ2  is specified by  𝑤=(−1,3)  and  𝑏=−6 .
-What label,  1  or  −1 , is assigned to the point  (1,1) ?
-'''
+# A linear classifier on  ℝ2  is specified by  𝑤=(−1,3)  and  𝑏=−6 .
+# What label,  1  or  −1 , is assigned to the point  (1,1) ?
 
-w = np.array([-3,1])
-x = np.array([1,1])
-b = -6
+w = np.array([1, 1, -3, 0])
+x = np.array([3, 1, 1, 4])
+b = -2
+y = 1
 
-result = w.dot(x)+b
+y  = w.dot(x) + b
+print('w.x + b =', y)
 
-print('result', result)
-
-if result > 0:
+# Test the classifier quickyly
+if y > 0:
     print('Classify as 1')
-else:
+elif y < 0:
     print('Classify as -1')
+else:
+    print('Classify as "Correct" or 0')
+
+# Calculate the loss when the result is less than 0
+if y <= 0:
+    loss = -y
+    print('The loss of the function is =', loss)
+else:
+    print('There was no loss')
